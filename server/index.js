19f17b81b0397bd5155adeea509a790accd6d0ad
ondre86 <ondre86@gmail.com>
2024-11-24 02:43:10 -0600

    ALPHA 0.4.1 (API query setup)

--- server/index.js
+++ server/index.js
@@ -39,12 +39,49 @@ app.listen(process.env.PORT, (error) =>{
     }
 )
 
-function sendResponse(req, res){
+async function sendResponse(req, res){
     res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL)
 
+    let clientRequest = JSON.parse(req.body)
 
+    clientRequest.client.location.ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+    await fetch(`http://ip-api.com/json/${clientRequest.client.location.ip.slice(7)}`)
+    .then(resp => {clientRequest.client.location.location = resp.body})
+
+    let query = ''
+    let spoonacularQuery = ''
+    let spoonacularNumber = 'number=100'
+
+    switch (clientRequest.request.mode) {
+        case 'pantry':
+            clientRequest.request.message.forEach(term => {
+                spoonacularQuery += `${term},+`
+                query += `"${term}"` + '+'
+            })
+            query = `recipes+with+` + query.slice(0, query.length-1)
+            spoonacularQuery = spoonacularQuery.slice(0, spoonacularQuery.length-2)
+
+            break
     
-    res.send({
-        received: req.body
-    })
+        case 'recipe':
+            query = `${clientRequest.request.message[0]}+recipes`
+            break
+        case 'extractor':
+            query = clientRequest.request.message[0]
+    }
+
+    console.log(query)
+    console.log(spoonacularQuery)
+
+    // search = await fetch(`https://api.search.brave.com/res/v1/web/search?q${query}`, {
+    //     headers: {
+    //         "X-Subscription-Token": process.env.BRAVE_SEARCH_API_KEY
+    //     }
+    // })
+
+    // console.log(search)
+
+    res.send(
+        JSON.stringify(clientRequest)
+    )
 }
--- src/stores/search.js
+++ src/stores/search.js
@@ -5,6 +5,7 @@ export const useSearchModeStore = defineStore('search', ()=>{
 	const searchMode = ref('pantry')
 	const searchTerms = ref(new Set([]))
 	const searchTermsForServer = ref(new Set([]))
+	const shoppingList = ref(new Set([]))
 	const submittedRequest = ref(null)
 	const requestFulfilled = ref(null)
 
@@ -27,6 +28,9 @@ export const useSearchModeStore = defineStore('search', ()=>{
 	function addServerSearchTerm(term){
 		searchTermsForServer.value.add(term)
 	}
+	function addItemToShoppingList(item){
+		shoppingList.value.add(item)
+	}
 	function deleteSearchTerm(term){
 		searchTerms.value.delete(term)
 	}
@@ -39,14 +43,63 @@ export const useSearchModeStore = defineStore('search', ()=>{
 	function clearServerSearchTerms(){
 		searchTermsForServer.value.clear()
 	}
+	function clearShoppingList(){
+		shoppingList.value.clear()
+	}
 
 	async function sendSearchTerms(mode){
 		submittedRequest.value = true
-		const values = Array.from(searchTermsForServer.value)
-		fetch(import.meta.env.VITE_SERVER_URL, {
+		const searchTermArray = Array.from(searchTermsForServer.value)
+		const shoppingListArray = Array.from(shoppingList.value)
+
+		await fetch(import.meta.env.VITE_SERVER_URL, {
             method: "POST",
             body: JSON.stringify({
-                message: values
+				client:{
+					location: {
+						ip: '',
+						location: ''
+					},
+
+					time: {
+						requestTimestamp: new Date().toUTCString(),
+						requestDate: new Date().toLocaleDateString(),
+						requestTime: new Date().toLocaleTimeString(),
+					},
+				
+					browser: {
+						browserVersion: navigator.userAgent,
+						browserLanguage: navigator.language,
+						browserOnline: navigator.onLine,
+						page: window.location.origin,
+						referrer: document.referrer,
+						previousSitesAmount: history.length,
+					},
+
+					storage: {
+						cookiesEnabled: navigator.cookieEnabled,
+						cookies: document.cookie.split(';'),
+						localStorage: localStorage
+					},
+
+					device: {
+						sizeScreenW: screen.width,
+						sizeScreenH: screen.height,
+						sizeDocW: document.width,
+						sizeDocH: document.height,
+						sizeInW: innerWidth,
+						sizeInH: innerHeight,
+						sizeAvailW: screen.availWidth,
+						sizeAvailH: screen.availHeight
+					}
+			
+				},
+				request:{
+					message: searchTermArray,
+					mode: searchMode.value,
+					shoppingList: shoppingListArray
+				},
+
             })
         })
         .then(response => response.json())
@@ -62,6 +115,7 @@ export const useSearchModeStore = defineStore('search', ()=>{
 		searchMode, 
 		searchTerms, 
 		searchTermsForServer, 
+		shoppingList,
 		getSearchMode, 
 		getSearchTerms, 
 		getServerSearchTerms, 
@@ -70,10 +124,12 @@ export const useSearchModeStore = defineStore('search', ()=>{
 		changeSearchMode, 
 		addSearchTerm, 
 		addServerSearchTerm, 
+		addItemToShoppingList,
 		deleteSearchTerm, 
 		deleteServerSearchTerm, 
 		clearSearchTerms,
 		clearServerSearchTerms,
+		clearShoppingList,
 		sendSearchTerms 
 	}
 })
