67f7766ef850019d7ee02f287b2482caa4d2dc2a
ondre86 <ondre86@gmail.com>
2024-12-11 11:58:16 -0600

    ALPHA 0.7.2 (auth persistence)

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,27 +1,26 @@
 import { useAuthStore } from "~/stores/auth"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+	if (process.server) return
+
     const userStore = useAuthStore()
 
-    // if (!userStore.isInitialized) {
-    //   console.log('Waiting for Firebase Auth initialization...')
-    //   await new Promise((resolve) => {
-    //     const unwatch = userStore.$onAction(({ name, after }) => {
-    //       if (name === 'monitorAuthState') {
-    //         after(() => {
-    //           unwatch()
-    //           resolve()
-    //         })
-    //       }
-    //     })
-    //   })
-    // }
+	
+	// Wait for Firebase initialization if not yet initialized
+	if (!userStore.isInitialized) {
+		console.log('Waiting for Firebase Auth initialization...');
+		await userStore.monitorAuthState(); // Wait for the auth state to load
 
+		if (!to.meta.requiresAuth) return
 
-    if (!to.meta.requiresAuth) return
+	}
 
-    if (to.meta.requiresAuth && !userStore.user) {
-      return navigateTo('/auth')
-    }
+	// Redirect to login if no user is authenticated
+	if (!userStore.user) {
+		console.log('User not authenticated, redirecting to login...');
+		return navigateTo({ name: 'auth' });
+	}
+
+	console.log('User authenticated, proceeding to route.');
   })
   
