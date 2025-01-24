dbf3a6c078f0f88d89e9ba9ca2a32f22aa747346
ondre86 <ondre86@gmail.com>
2025-01-24 16:34:26 -0600

    BETA 2.15 (Stripe #7)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/image'
   ],
+  alias: {
+    'string_decoder': 'string_decoder/',
+  },
   nitro: {
     preset: 'cloudflare-pages'
   },
