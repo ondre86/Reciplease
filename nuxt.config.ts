e46f680ad3d65ca172f7b3762c0501a76a8b467a
ondre86 <ondre86@gmail.com>
2025-01-26 09:40:23 -0600

    BETA 2.15 (Stripe #26)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,9 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxt/image'
   ],
+  nitro: {
+    preset: 'cloudflare_pages'
+  },
   ssr: true,
   routeRules: {
     "/search": { ssr: false },
